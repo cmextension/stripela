b4f24d6f61b739a5805bc01c778f593054347794
Hung Tran <nguyentieuhau@gmail.com>
2021-08-29 12:38:19 +0700

    Customers

---
+++ administrator/components/com_stripela/controllers/customer.json.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * @package     Stripela
+ * @subpackage  com_stripela
+ * @copyright   Copyright (C) 2021 CMExtension
+ * @license     GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Response\JsonResponse;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\HTML\HTMLHelper;
+
+require_once __DIR__ . '/base.php';
+
+/**
+ * Controller for customer.
+ *
+ * @package     Stripela
+ * @subpackage  com_stripela
+ * @since       1.0.0
+ */
+class StripelaControllerCustomer extends StripelaControllerBase
+{
+	/**
+	 * Get customers.
+	 *
+	 * @since  1.0.0
+	 */
+	public function getCustomers()
+	{
+		$data = [];
+
+		$config = ComponentHelper::getParams('com_stripela');
+		$secretKey = $config->get('stripe_secret_key');
+		$limit = $config->get('limit', 20);
+
+		$input = $this->input;
+		$startingAfter = $input->get('starting_after');
+		$endingBefore = $input->get('ending_before');
+		$email = $input->get('email');
+
+		$stripe = new \Stripe\StripeClient($secretKey);
+
+		$params = ['limit' => $limit];
+
+		if ($startingAfter)
+			$params['starting_after'] = $startingAfter;
+
+		if ($endingBefore)
+			$params['ending_before'] = $endingBefore;
+
+		if ($email)
+			$params['email'] = $email;
+
+		try {
+			$response = $stripe->customers->all($params);
+		} catch (Exception $e) {
+			echo new JsonResponse(null, $e->getMessage(), true);
+
+			return false;
+		}
+
+		$customers = [];
+		$newStartingAfter = '';
+		$newEndingBefore = '';
+
+		if (count($response->data) > 0)
+		{
+			foreach ($response->data as $customer)
+			{
+				$customers[] = [
+					'id'			=> $customer->id,
+					'name'			=> $customer->name,
+					'created'		=> HTMLHelper::_('stripela.date', $customer->created),
+					'description'	=> $customer->description,
+					'email'			=> $customer->email,
+				];
+			}
+
+			$first = $customers[0];
+			$last = $customers[count($customers) - 1];
+
+			if (!$startingAfter && !$endingBefore && $response->has_more)
+			{
+				$newStartingAfter = $last['id'];
+			}
+			else
+			{
+				if ($startingAfter)
+				{
+					if ($response->has_more)
+						$newStartingAfter = $last['id'];
+	
+					$newEndingBefore = $first['id'];
+				}
+	
+				if ($endingBefore)
+				{
+					if ($response->has_more)
+						$newEndingBefore = $first['id'];
+	
+					$newStartingAfter = $last['id'];
+				}
+			}
+		}
+		else
+		{
+			if ($startingAfter)
+				$newEndingBefore = $startingAfter;
+		}
+
+		$data['items'] = $customers;
+		$data['starting_after'] = $newStartingAfter;
+		$data['ending_before'] = $newEndingBefore;
+		
+		echo new JsonResponse($data);
+
+		return true;
+	}
+
+	/**
+	 * Get customer detail.
+	 *
+	 * @since  1.0.0
+	 */
+	public function getCustomer()
+	{
+		$config = ComponentHelper::getParams('com_stripela');
+		$secretKey = $config->get('stripe_secret_key');
+
+		$input = $this->input;
+		$customerId = $input->get('id');
+
+		if (empty($customerId))
+		{
+			echo new JsonResponse(null, Text::_('COM_STRIPELA_NO_CUSTOMER_ID'), true);
+
+			return false;
+		}
+
+		$stripe = new \Stripe\StripeClient($secretKey);
+
+		try {
+			$r = $stripe->customers->retrieve($customerId);
+		} catch (Exception $e) {
+			echo new JsonResponse(null, $e->getMessage(), true);
+
+			return false;
+		}
+
+		$customer = [
+			'id'			=> $r->id,
+			'name'			=> $r->name,
+			'email'			=> $r->email,
+			'currency'		=> strtoupper($r->currency),
+			'created'		=> HTMLHelper::_('stripela.date', $r->created),
+			'description'	=> $r->description,
+			'phone'			=> $r->phone,
+			'address'		=> $r->address,
+		];
+
+		echo new JsonResponse($customer);
+
+		return true;
+	}
+}
--- administrator/components/com_stripela/views/dashboard/tmpl/default_customers.php
+++ administrator/components/com_stripela/views/dashboard/tmpl/default_customers.php
@@ -14,20 +14,269 @@ use Joomla\CMS\Language\Text;
 	var Customers = Vue.extend({
 		data: function() {
 			return {
-				loading: true,
+				loadingList: true,
+				customers: [],
+				starting_after: '',
+				ending_before: '',
+				filter_email: '',
+				dialog: false,
+				loadingDetail: true,
+				customerDetailError: '',
+				customer: null,
 			}
 		},
 		template: '#customers',
-		computed: {},
-		methods: {}
+		watch: {
+			dialog: function(val) {
+				if (!val)
+					this.customer = null
+			}
+		},
+		methods: {
+			formatDate: function(date) {
+				if (!date) return null
+
+				const [year, month, day] = date.split('-')
+
+				if (dateFormat == 'Y/m/d')		return `${year}/${month}/${day}`
+				else if (dateFormat == 'Y-m-d')	return `${year}-${month}-${day}`
+				else if (dateFormat == 'd/m/Y')	return `${day}/${month}/${year}`
+				else if (dateFormat == 'd-m-Y')	return `${day}-${month}-${year}`
+				else if (dateFormat == 'm/d/Y')	return `${month}/${day}/${year}`
+				else if (dateFormat == 'm-d-Y')	return `${month}-${day}-${year}`
+				else if (dateFormat == 'd.m.Y')	return `${day}.${month}.${year}`
+				else if (dateFormat == 'Y.m.d')	return `${year}.${month}.${day}`
+			},
+			filter: function() {
+				this.starting_after = ''
+				this.ending_before = ''
+				this.getCustomers()
+			},
+			clearFilter: function() {
+				this.starting_after = ''
+				this.ending_before = ''
+				this.filter_email = ''
+				this.getCustomers()
+			},
+			getCustomers: function(directionKey) {
+				let _this = this
+				let url = componentRoute + '&task=customer.getCustomers&' + token + '=1'
+
+				if (directionKey == -1)
+					url +=  '&ending_before=' + _this.ending_before
+
+				if (directionKey == 1)
+					url +=  '&starting_after=' + _this.starting_after
+
+				if (_this.filter_email)
+					url += '&email=' + _this.filter_email
+
+				_this.loadingList = true
+
+				$.ajax({
+					url: url,
+					method: 'GET',
+					dataType: 'json',
+					success: function(r) {
+						_this.loadingList = false
+
+						if (!r.success)
+							return
+
+						_this.customers = r.data.items
+						_this.starting_after = r.data.starting_after
+						_this.ending_before = r.data.ending_before
+					}
+				})
+			},
+			getCustomerDetail: function(id) {
+				let url = componentRoute + '&task=customer.getCustomer&' + token + '=1&id=' + id
+				this.loadingDetail = true
+				this.customerDetailError = ''
+				this.dialog = true
+
+				let _this = this
+
+				$.ajax({
+					url: url,
+					method: 'GET',
+					dataType: 'json',
+					success: function(r) {
+						if (r.message)
+							_this.customerDetailError = r.message
+
+						if (r.success && r.data)
+							_this.customer = r.data
+
+						_this.loadingDetail = false
+					}
+				})
+			}
+		},
+		mounted: function() {
+			this.getCustomers()
+		}
 	})
 </script>
 <script type="text/x-template" id="customers">
 	<div>
 		<h2><?php echo Text::_('COM_STRIPELA_CUSTOMERS') ; ?></h2>
 
-		<div v-show="loading" class="text-center">
+		<v-divider></v-divider>
+
+		<div v-show="loadingList" class="text-center">
 			<v-progress-circular indeterminate></v-progress-circular>
+		</div>
+
+		<div v-show="!loadingList">
+			<v-dialog
+				v-model="dialog"
+				width="600px"
+			>
+				<v-card>
+					<v-card-title>
+						<span class="text-h5">
+							<span v-if="customer && customer.name">{{ customer.name }}</span>
+							<span v-else-if="customer && customer.email">{{ customer.email }}</span>
+							<span v-else-if="customer && customer.id">{{ customer.id }}</span>
+						</span>
+					</v-card-title>
+					<v-card-text>
+						<v-alert
+							v-show="customerDetailError"
+							type="error"
+							dense
+							outlined
+						>
+							{{ customerDetailError }}
+						</v-alert>
+						<div v-show="loadingDetail" class="text-center">
+							<v-progress-circular indeterminate></v-progress-circular>
+						</div>
+
+						<v-simple-table v-if="customer !== null">
+							<template v-slot:default>
+							<tbody>
+								<tr>
+									<th>ID</th>
+									<td>{{ customer.id }}</td>
+								</tr>
+								<tr v-show="customer.name">
+									<th><?php echo Text::_('COM_STRIPELA_NAME'); ?></th>
+									<td>{{ customer.name }}</td>
+								</tr>
+								<tr v-show="customer.email">
+									<th><?php echo Text::_('COM_STRIPELA_EMAIL'); ?></th>
+									<td>{{ customer.email }}</td>
+								</tr>
+								<tr v-show="customer.description">
+									<th><?php echo Text::_('COM_STRIPELA_DESCRIPTION'); ?></th>
+									<td>{{ customer.description }}</td>
+								</tr>
+								<tr v-show="customer.currency">
+									<th><?php echo Text::_('COM_STRIPELA_CURRENCY'); ?></th>
+									<td>{{ customer.currency }}</td>
+								</tr>
+								<tr v-show="customer.address">
+									<th><?php echo Text::_('COM_STRIPELA_ADDRESS'); ?></th>
+									<td>{{ customer.address }}</td>
+								</tr>
+								<tr v-show="customer.phone">
+									<th><?php echo Text::_('COM_STRIPELA_PHONE'); ?></th>
+									<td>{{ customer.phone }}</td>
+								</tr>
+								<tr>
+									<th><?php echo Text::_('COM_STRIPELA_CREATED'); ?></th>
+									<td>{{ customer.created }}</td>
+								</tr>
+							</tbody>
+							</template>
+						</v-simple-table>
+					</v-card-text>
+					<v-card-actions>
+						<v-spacer></v-spacer>
+						<v-btn
+							@click="dialog = false"
+						>
+							<?php echo Text::_('COM_STRIPELA_CLOSE'); ?>
+						</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-dialog>
+
+			<v-row class="mb-2">
+				<v-col
+					class="pb-0"
+					cols="12"
+					sm="6"
+					md="6"
+				>
+					<v-text-field
+						v-model="filter_email"
+						label="Email"
+						prepend-icon="fas fa-user fa-fw"
+					></v-text-field>
+				</v-col>
+
+				<v-col
+					cols="12"
+					sm="6"
+					md="6"
+					class="d-flex align-content-center flex-wrap"
+				>
+					<v-btn
+						v-on:click="filter()"
+						color="primary"
+					>
+						<?php echo Text::_('COM_STRIPELA_FILTER'); ?>
+					</v-btn>
+					<v-btn
+						v-on:click="clearFilter()"
+					>
+						<?php echo Text::_('COM_STRIPELA_CLEAR'); ?>
+					</v-btn>
+				</v-col>
+			</v-row>
+
+			<v-simple-table>
+				<template v-slot:default>
+					<thead>
+						<tr>
+							<th class="text-left"><?php echo Text::_('COM_STRIPELA_NAME'); ?></th>
+							<th class="text-left"><?php echo Text::_('COM_STRIPELA_EMAIL'); ?></th>
+							<th class="text-left"><?php echo Text::_('COM_STRIPELA_DESCRIPTION'); ?></th>
+							<th class="text-left"><?php echo Text::_('COM_STRIPELA_CREATED'); ?></th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="item in customers" :key="item.id">
+							<td>{{ item.name }}</td>
+							<td>{{ item.email }}</td>
+							<td>{{ item.description }}</td>
+							<td>{{ item.created }}</td>
+							<td>
+								<v-btn
+									x-small
+									@click="getCustomerDetail(item.id)"
+								>
+									<v-icon x-small>fas fa-eye</v-icon>
+								</v-btn>
+							</td>
+						</tr>
+					</tbody>
+				</template>
+			</v-simple-table>
+
+			<div class="stripela-pagination float-right">
+				<v-btn v-if="ending_before" v-on:click="getCustomers(-1)">
+					<v-icon>fas fa-chevron-left</v-icon>
+				</v-btn>
+
+				<v-btn v-if="starting_after" v-on:click="getCustomers(1)">
+					<v-icon>fas fa-chevron-right</v-icon>
+				</v-btn>
+			</div>
 		</div>
 	</div>
 </script>
